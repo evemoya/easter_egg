65783f5f8f28b3acd8d141f7b2bda2207eaa8b24
Everet moya <everette_333@hotmail.com>
2021-04-17 14:27:49 -0400

    subida de sitio

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+
+//Funcion de Menu - Boton @ver menu oculta  o muestra
+var menuBtn = $('.menu-icon');
+var actmenu = $('.navigation ul');
+
+menuBtn.click(function(){
+
+  if(actmenu.hasClass ('show') ) {
+    actmenu.removeClass('show');
+  } else{
+    actmenu.addClass('show');
+
+  }
+
+
+  });
+
+
+
+// Funciones de contacto  
+// N1 Enviar informacion a sitio
+
+function enviar() { // Envia los datos de formulario a la direccion de correo * Incompleta
+  alert('se ha enviado tu comentario, muchas gracias')
+}
+
+
+// Limpiar - limia el campo Target
+var ultimo = null;
+function marcar(elemento) {
+  ultimo = elemento;
+}
+
+
+function limpiar() {
+  if (ultimo != null) {
+  	ultimo.value = "";
+  }
+}
+
+
